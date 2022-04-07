db655b3b17a1f0254f413ece681d337276df0009
Spencer Kwon <spencerkwon92@gmail.com>
2022-04-06 16:21:28 -1000

    Started to building for Treemap

--- treeMap/Node.java
+++ treeMap/Node.java
@@ -1,15 +1,67 @@
 package univ.cis.cs490.treeMap;
 
-public class Node {
+import java.awt.*;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
-    Node parent;
-    int size;
-    String fileName;
+enum Direction{
+    HORIZENTAL,
+    VIRTICAL
+}
 
-    Node(String fn, int size, Node pn){
-        this.parent = pn;
-        this.size = size;
-        this.fileName = fn;
+class Node {
 
+    List<Node> fileList;
+    String fileName, address;
+    private double size;
+    Direction direction;
+
+    Node(File file){
+        fileList = new ArrayList<>();
+        this.fileName = file.getName();
+        this.address = file.getAbsolutePath();
+        if(file.isFile()){
+            this.size = file.length();
+        }else{
+            for(File ele:file.listFiles()){
+                Node child = new Node(ele);
+                if(this.direction == Direction.VIRTICAL){
+                    child.direction = Direction.HORIZENTAL;
+                }else{
+                    child.direction = Direction.VIRTICAL;
+                }
+                this.size +=child.size;
+                fileList.add(child);
+            }
+        }
     }
+
+    public void setSize(double s){
+        this.size = s;
+    }
+
+    public double getSize(){
+        return size;
+    }
+
+    public void draw(double xPos, double yPos, double width, double hight, Graphics2D g){
+        double ratio = width / size;
+        double startPoint = xPos;
+        if(fileList.size() == 0){
+            g.drawRect((int) xPos, (int) yPos, (int) width, (int) hight);
+        }else{
+            for(Node ele:fileList){
+                if(direction == Direction.VIRTICAL){
+                    double childWidth = ele.size / ratio;
+                    ele.draw(startPoint, yPos,childWidth, hight, g);
+                    startPoint += childWidth;
+
+                }else if(direction == Direction.VIRTICAL){
+
+                }
+            }
+        }
+    }
+
 }
--- treeMap/Visual.java
+++ treeMap/Visual.java
@@ -1,19 +1,26 @@
 package univ.cis.cs490.treeMap;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Visual extends JPanel {
-    List<Node> tree = new ArrayList<>();
-
+class Visual extends JPanel {
+//    List<Node> tree = new ArrayList<>();
     Visual(){
-
+        String testFile = "/Users/sungjin.spencerkwon/Desktop/univ";
+        File f = new File(testFile);
+        Node parNode = new Node(f);
+        parNode.direction = Direction.HORIZENTAL;
     }
 
-    public List<Node> createTreeMap(){
-        List<Node> tree = new ArrayList<>();
+    @Override
+    public void paintComponent(Graphics g1){
+        Graphics2D g = (Graphics2D)g1;
+    }
 
-        return tree;
+    public void sizeTransform(double data){
+
     }
 }
